32a0a9ddedb09bee59fb9ecf856c06c24b237f54
Viji Justin <viji.justin@planmill.com>
2020-09-22 16:11:14 +0300

    as per the dt2js changes

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const schemaEndpoint = ".schema";
 const indexSubDir = "api_docs"
 
 var linkItems = [
-  "absence1_5:array",
-  "absence_single1_5:single",
-  "account1_5:array",
-  "account_single1_5:single"
+  "absence1_5:array:noAdditionalProperties",
+  "absence_single1_5:single:noAdditionalProperties",
+  "account1_5:array:noAdditionalProperties",
+  "account_single1_5:single:noAdditionalProperties"
 ];
 
 // serve files from the public directory
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 });
 
 //const currentDir = process.env.LAMBDA_TASK_ROOT;
-const currentDir = __dirname;
+const currentDir = __dirname +'/api_docs/' ;
 
 const main = async function () {
 	let filePath;
@@ -39,20 +39,19 @@ const main = async function () {
 		linkItems.forEach(async  (elem) => 
 		{
 			try {
-				var name = elem.split(":")
+				var name = elem.split(":");
 				filePath = join(currentDir, name[0] + ramlEndpoint);
-				
 				let ramlData = fs.readFileSync(filePath).toString();
-				let schema = await r2j.dt2js(ramlData, name[0].replace('1_5',''));
+				let isArrayType = (name[1] === 'array');
+				let typeName = name[0].replace('1_5','');
+				let additionalProperties = (name[2] === 'noAdditionalProperties'? false : true);
+				let schema = await r2j.dt2js(ramlData, typeName, {addType: true, isArray: isArrayType, additionalProperties :additionalProperties});
 				let schemaStr = JSON.stringify(schema, null, 2);
-				if(name[1] === 'array') {
-					var replaceString = '"$ref": "#/definitions/'+name[0].replace('1_5','')+'"';
-					schemaStr = schemaStr.replace(replaceString, '"oneOf": [{"type": "array","items": { '+replaceString+' }},{'+replaceString+'}  ]');
-				}
-				let jsonfilePath = join(currentDir, '../../api_docs/' + name[0]+ schemaEndpoint);
+				
+				let jsonfilePath = join(currentDir, name[0]+ schemaEndpoint);
 				fs.writeFile(jsonfilePath, schemaStr, function (err, data) {});
   			  } catch (e) {
-			  console.log(e);
+				console.log(e);
 			  }
 		})
 		return;
